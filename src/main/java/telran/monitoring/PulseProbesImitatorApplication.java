64152d3b0cadb1a9eb2b4993245bd31c25a71f71
Yuragranovsky <yuragranovsky@gmail.com>
2023-03-11 18:18:02 +0200

    pulse imitator empty project

---
+++ src/main/java/telran/monitoring/PulseProbesImitatorApplication.java
@@ -0,0 +1,13 @@
+package telran.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseProbesImitatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseProbesImitatorApplication.class, args);
+	}
+
+}
